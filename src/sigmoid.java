386c990c38e3d155e6c8c0068c8994fa565d3a69
itaypld <itay.pld@gmail.com>
2025-04-08 14:07:45 +0300

    derivitive for sigmoid function

--- src/sigmoid.java
+++ src/sigmoid.java
@@ -12,5 +12,8 @@ public class sigmoid implements activation {
         // For example, using the sigmoid activation function
         return 1 / (1 + Math.exp(-x));
     }
-    
+    public double derivative(double sigmoidOutput) {
+        // sigmoidOutput is assumed to be σ(x), not the raw x
+        return sigmoidOutput * (1 - sigmoidOutput);
+    }
 }
